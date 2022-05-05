4dffbf57fccd7577329c738db94c2aec186b20d4
Garden-of-Eden-pattern <101173362+Garden-of-Eden-pattern@users.noreply.github.com>
2022-05-05 23:05:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { IntersectingCirclesSpinner } from 'epic-spinners'
 
 window.onload = function(){
-  const spinnerBlock = document.getElementByClassName('intersecting-circles-spinner .spinnerBlock');
+  const spinnerBlock = document.getElementByClassName('intersecting-circles-spinner');
   spinnerBlock.classList.add('loaded');
 }
 
