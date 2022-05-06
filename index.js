2f7513ff12a5596dcc0e4cc27a9356fbc9f6cec5
Garden-of-Eden-pattern <dyuia0303@icloud.com>
2022-05-06 17:37:11 +0900

    added vue.js, which is framework, and updated files

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+import { IntersectingCirclesSpinner } from 'epic-spinners'
+
 window.onload = function(){
   const intersecting-circles-spinner = document.getElementById('loading');
   intersecting-circles-spinner.classList.add('loaded');
 }
 
+<intersecting-circles-spinner
+  :animation-duration="1200"
+  :size="70"
+  color="#ff1d5e"
+/>
